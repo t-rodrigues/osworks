a7668462e8915676edd3c4f197488ee3d4f5609c
Thiago Rodrigues <thiagor_@live.com>
2020-10-23 18:10:58 -0300

    feat: set timezone to UTC

--- src/main/java/com/thiagorodrigues/osworks/OsworksApplication.java
+++ src/main/java/com/thiagorodrigues/osworks/OsworksApplication.java
@@ -1,10 +1,19 @@
 package com.thiagorodrigues.osworks;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OsworksApplication {
+
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(OsworksApplication.class, args);
